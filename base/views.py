e7881933486f36579aa00febfa8b06b06266e20b
Dhineshwaran-C <dhineshwaran02@gmail.com>
2023-10-28 12:32:07 +0530

    Updated On Oct 28th

--- base/views.py
+++ base/views.py
@@ -256,7 +256,7 @@ def paymentsuccess(request):
         qrfeatures = qrcode.QRCode(version=1,box_size=40,border=3)
 
         qr = qrcode.make(id)
-        qr.save('qrimages/'+user.email+'.png')
+        qr.save('qrimages/'+user.regno+'.png')
 
 
 
@@ -265,17 +265,35 @@ def paymentsuccess(request):
         <html>
         <body>
         <h1 style="color:purple;"><b>REVOTRONICS</b></h1>
-        <h2 style="font-family:sans-serif;color:blue">Thank you for registering for Revotronics.</h2><br>
-        <h3 style="font-family:sans-serif;">Please find the QR code attached to this email for check-in on the day of symposium. Stay tuned for further updates.</h3>
-        <h3 style="font-family:sans-serif;">Get, Set, REV!</h3>
-        <h3 style="font-family:sans-serif;">Best Regards,</h3>
-        <h3 style="font-family:sans-serif;color:purple">Team Revotronics.</h3>
+        <h2 style="font-family:sans-serif;color:blue">Thank you for registering REVOTRONICS 2023! We're excited to have you join us for this upcoming symposium.</h2>
+        <h3 style="font-family:sans-serif;">Please find the QR code attached to this email, which will be your key to a seamless check-in process on the day of the event.</h3><br>
+        <h3 style="font-family:sans-serif;">
+        <b>Here are some important details about the symposium:</b><br>
+        Date: 04 November 2023<br>
+        Reporting Time: 8:00 AM (Please note that the college gate will close promptly at 8:00 AM, so ensure you arrive on time.)<br>
+        Dress Code: We kindly request all attendees to maintain proper dress code for the event.<br>
+        Don't forget your ID cards; they are essential for identification and entry.
+        </h3><br>
+        <h3 style="font-family:sans-serif;">
+        <b>Location:</b><br>
+        REC CAMPUS:<br>
+        [Click here for directions](https://maps.app.goo.gl/gyf1YiuxrjEKj99KA)
+        </h3>
+        <h3 style="font-family:sans-serif;">"Let's set screens on fire..." Get ready to REV up your knowledge!</h3>
+        <h3 style="font-family:sans-serif;color:purple">If you have any questions or need further information, please feel free to reach out to us. We're here to assist you in any way we can.</h3>
+        <h3 style="font-family:sans-serif;">
+        Thanks and regards,<br>
+        Team Revotronics 2023<br>
+        Department of ELECTRONICS AND COMMUNICATION ENGINEERING<br>
+        RAJALAKSHMI ENGINEERING COLLEGE
+        </h3>
         </body>
         </html>
         """
 
+
         yag = yagmail.SMTP('revotronics23@gmail.com','xxux jqja ouct swkk')
-        yag.send(user.email,'Revotronics Payment Successful',content,'qrimages/'+user.email+'.png')
+        yag.send(user.email,'Revotronics Payment Successful',content,'qrimages/'+user.regno+'.png')
         yag.close()
 
     else:
